951b90c3cb389b56414210afe625a496faf1ad8b
kunal <kunalrana.niit@gmail.com>
2017-11-15 15:34:44 +0530

    first commit

---
+++ server/api/moviesendpoint/moviesendpoint.model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+var MoviesendpointSchema = new mongoose.Schema({
+  MovieName: String,
+Year: String,
+
+Poster: String,
+
+Title: String,
+StarCast: String,
+Duration: String,
+Language: String,
+Genre: String,
+Overview: String
+});
+
+export default mongoose.model('Movies', MoviesendpointSchema);
